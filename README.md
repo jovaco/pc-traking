43e1f068ad0ffacc2f85c2919774233f34a26651
bredouane <billel.redouane@gmail.com>
2016-08-21 11:42:16 -0400

    init project with jbossForge

---
+++ README.md
@@ -0,0 +1,4 @@
+# bug-traking
+Exemple d'application web Java avec une architecture simple et adaptable au mobile en JPA + CDI + Transactional + JSF + Primeface.
+
+# Work in progress...
---
+++ src/main/java/org/pc/traking/model/UserRoles.java
@@ -0,0 +1,95 @@
+package org.pc.traking.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+
+@Entity
+public class UserRoles implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, nullable = false)
+	private Long id;
+	@Version
+	@Column(name = "version")
+	private int version;
+
+	@Column
+	private String username;
+
+	@Column(length = 32)
+	private String role;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(final Long id) {
+		this.id = id;
+	}
+
+	public int getVersion() {
+		return this.version;
+	}
+
+	public void setVersion(final int version) {
+		this.version = version;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof UserRoles)) {
+			return false;
+		}
+		UserRoles other = (UserRoles) obj;
+		if (id != null) {
+			if (!id.equals(other.id)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		String result = getClass().getSimpleName() + " ";
+		if (username != null && !username.trim().isEmpty())
+			result += "username: " + username;
+		if (role != null && !role.trim().isEmpty())
+			result += ", role: " + role;
+		return result;
+	}
+}
